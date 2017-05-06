e155d5310a9468a15fcdfa3dcc854668141c8496
David Spickett <spickettdavid@googlemail.com>
2017-05-06 16:09:50 +0100

    More variable renaming.

--- main.py
+++ main.py
@@ -12,16 +12,14 @@ pygame.init()
 clock = pygame.time.Clock() #Clock to limit frames per second
 framesPerSecond = 30        #We want to run the game at 30fps
 
-boardWidth  = 100
-boardHeight = 50
+BOARD_WIDTH  = 100
+BOARD_HEIGHT = 50
 TILE_SIZE    = 10
 
-swidth  = boardWidth*TILE_SIZE
-sheight = boardHeight*TILE_SIZE
+SCREEN_WIDTH  = BOARD_WIDTH*TILE_SIZE
+SCREEN_HEIGHT = BOARD_HEIGHT*TILE_SIZE
 
-font = pygame.font.Font(pygame.font.get_default_font(),20)
-
-screen = pygame.display.set_mode((swidth,sheight))
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Maze Game')
 
 FLOOR = 0 
@@ -35,7 +33,7 @@ PLAYEER_COLOUR = (255,0,0)
 MAZE_COLOURS = [FLOOR_COLOUR, WALL_COLOUR, PATH_COLOUR]
 
 def mazeArray(value):
-    return [[value]*boardHeight for i in range(boardWidth)]
+    return [[value]*BOARD_HEIGHT for i in range(BOARD_WIDTH)]
 
 class maze(): #The main maze class
     def __init__(self):
@@ -43,20 +41,18 @@ class maze(): #The main maze class
         self.visited = mazeArray(0)
         self.neighbours = []
         self.stack = []
-        
-        #Used for generation
-        self.currentX = 0
-        self.currentY = 0
+        self.x = 0
+        self.y = 0
 
     def ClearPath(self):
-        for i in range(boardWidth):
-            for j in range(boardHeight):
+        for i in range(BOARD_WIDTH):
+            for j in range(BOARD_HEIGHT):
                 if self.layout[i][j] == PATH:
                     self.layout[i][j] = FLOOR
         
     def drawMaze(self):
-        for i in range(boardWidth):
-            for j in range(boardHeight):
+        for i in range(BOARD_WIDTH):
+            for j in range(BOARD_HEIGHT):
                 cell_type = self.layout[i][j]
                 screen.fill(MAZE_COLOURS[cell_type], (i*TILE_SIZE,j*TILE_SIZE,TILE_SIZE,TILE_SIZE))
         
@@ -68,36 +64,36 @@ class maze(): #The main maze class
         #Do one step of generating a layout.
         
         #Pick a cell and mark it as part of the maze
-        self.layout[self.currentX][self.currentY] = FLOOR
+        self.layout[self.x][self.y] = FLOOR
         #Mark as visited
-        self.visited[self.currentX][self.currentY] = True
+        self.visited[self.x][self.y] = True
         
         self.neighbours = [] #Reset neighbours
         
         #Look for unvisited neighbours    
         try:
-            if (self.currentY-2) >= 0:
-                if not self.visited[self.currentX][self.currentY-2]: #Above
-                    self.neighbours.append((self.currentX,self.currentY-2))
+            if (self.y-2) >= 0:
+                if not self.visited[self.x][self.y-2]: #Above
+                    self.neighbours.append((self.x,self.y-2))
         except IndexError:
             pass
         
         try:
-            if (self.currentX-2) >= 0:
-                if not self.visited[self.currentX-2][self.currentY]: #Left
-                    self.neighbours.append((self.currentX-2,self.currentY))
+            if (self.x-2) >= 0:
+                if not self.visited[self.x-2][self.y]: #Left
+                    self.neighbours.append((self.x-2,self.y))
         except IndexError:
             pass
         
         try:
-            if not self.visited[self.currentX+2][self.currentY]: #Right
-                self.neighbours.append((self.currentX+2,self.currentY))
+            if not self.visited[self.x+2][self.y]: #Right
+                self.neighbours.append((self.x+2,self.y))
         except IndexError:
             pass
         
         try:
-            if not self.visited[self.currentX][self.currentY+2]: #Down
-                self.neighbours.append((self.currentX,self.currentY+2))
+            if not self.visited[self.x][self.y+2]: #Down
+                self.neighbours.append((self.x,self.y+2))
         except IndexError:
             pass
         
@@ -106,31 +102,31 @@ class maze(): #The main maze class
             newCell = self.neighbours[random.randint(0,len(self.neighbours)-1)]
         
             #Add current cell to the stack
-            self.stack.append((self.currentX,self.currentY))
+            self.stack.append((self.x,self.y))
                      
             #Remove the wall between current and chosen cell
             newX, newY = newCell
         
             #Cell is above
-            if newX == self.currentX and (newY+2) == self.currentY:
+            if newX == self.x and (newY+2) == self.y:
                 self.layout[newX][newY+1] = FLOOR
             #Cell is below
-            elif newX == self.currentX and (newY-2) == self.currentY:
+            elif newX == self.x and (newY-2) == self.y:
                 self.layout[newX][newY-1] = FLOOR
             #Cell is left
-            elif (newX+2) == self.currentX and newY == self.currentY:
+            elif (newX+2) == self.x and newY == self.y:
                 self.layout[newX+1][newY] = FLOOR
             #Cell is right
-            elif (newX-2) == self.currentX and newY == self.currentY:
+            elif (newX-2) == self.x and newY == self.y:
                 self.layout[newX-1][newY] = FLOOR
             
-            self.currentX = newX
-            self.currentY = newY
+            self.x = newX
+            self.y = newY
             
         else: #Cell has no neighbours
             if self.stack:
                 #Go back to the previous current cell
-                self.currentX, self.currentY = self.stack.pop() 
+                self.x, self.y = self.stack.pop() 
                 
             if not self.stack:
                 #Generation has finished
@@ -165,7 +161,7 @@ class player(): #The player
                     self.y -= 1
         
         if key[pygame.K_DOWN]:
-            if self.y != (boardHeight-1):
+            if self.y != (BOARD_HEIGHT-1):
                 if newMaze.layout[self.x][self.y+1] == FLOOR or newMaze.layout[self.x][self.y+1] == PATH:
                     newMaze.layout[self.x][self.y] = PATH
                     self.y += 1
@@ -177,7 +173,7 @@ class player(): #The player
                     self.x -= 1
         
         if key[pygame.K_RIGHT]:
-            if self.Y != (boardWidth-1):
+            if self.Y != (BOARD_WIDTH-1):
                 if newMaze.layout[self.x+1][self.y] == FLOOR or newMaze.layout[self.x+1][self.y] == PATH:
                     newMaze.layout[self.x][self.y] = PATH
                     self.x += 1
@@ -227,31 +223,21 @@ class mazeSolver(): #Assists in solving the maze
         newMaze.layout[self.currentX][self.currentY] = PATH
         
         #Look for open cells    
-        try:
-            if (self.currentY-1) >= 0:
-                if newMaze.layout[self.currentX][self.currentY-1] == FLOOR: #Above
-                    self.neighbours.append((self.currentX,self.currentY-1))
-        except:
-            print self.neighbours
+        if (self.currentY-1) >= 0:
+            if newMaze.layout[self.currentX][self.currentY-1] == FLOOR: #Above
+                self.neighbours.append((self.currentX,self.currentY-1))
+    
+        if (self.currentX-1) >= 0:
+            if newMaze.layout[self.currentX-1][self.currentY] == FLOOR: #Left
+                self.neighbours.append((self.currentX-1,self.currentY))
         
-        try:
-            if (self.currentX-1) >= 0:
-                if newMaze.layout[self.currentX-1][self.currentY] == FLOOR: #Left
-                    self.neighbours.append((self.currentX-1,self.currentY))
-        except:
-            print self.neighbours
-        
-        try:
+        if (self.currentX < BOARD_WIDTH):
             if newMaze.layout[self.currentX+1][self.currentY] == FLOOR: #Right
                 self.neighbours.append((self.currentX+1,self.currentY))
-        except:
-            print self.neighbours
-        
-        try:
+
+        if (self.currentY < BOARD_HEIGHT):
             if newMaze.layout[self.currentX][self.currentY+1] == FLOOR: #Down
                 self.neighbours.append((self.currentX,self.currentY+1))
-        except:
-            print self.neighbours
         
         if self.neighbours: #If we found some neighbours that are open
             newX, newY = self.neighbours[0] #Take the first option
